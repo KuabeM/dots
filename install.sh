d8606fa34e458e630325101f72b7cb7d011d98d7
Korbinian Maier <korbinian.maier@posteo.de>
2021-06-05 23:37:23 +0200

    Properly link all files and folders in install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,15 @@ for target in $targets; do
 done
 
 echo 'Setup config folders in .config'
-folders=(alacritty htop mako nvim pwio sway waybar)
+folders=(alacritty htop mako piow sway waybar)
 for folder in $folders; do
-  ln -nfs $HOME/.dots/$folder $HOME/.config/target
+  ln -nfs $HOME/.dots/$folder $HOME/.config/$folder
 done
+
+# Starship is directly in ~/.config
+ln -nfs $HOME/.dots/starship.toml $HOME/.config/starship.toml
+# Move nvim separately
+ln -nfs $HOME/.dots/nvim/init.vim $HOME/.config/nvim/init.vim
 
 echo 'Install vim plugins'
 nvim +PlugUpgrade +PlugUpdate +qall
