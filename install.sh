8151dbc7e17560bd8226e87df86d947fcfc46e3a
Korbinian Maier <korbinian.maier@franka.de>
2021-06-27 00:02:15 +0200

    Install script: create neovim dir and install plugin manager

--- install.sh
+++ install.sh
@@ -15,8 +15,12 @@ done
 # Starship is directly in ~/.config
 ln -nfs $HOME/.dots/starship.toml $HOME/.config/starship.toml
 # Move nvim separately
+mkdir -p $HOME/.config/nvim
 ln -nfs $HOME/.dots/nvim/init.vim $HOME/.config/nvim/init.vim
 
+echo 'Install neovim plugin manager'
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 echo 'Install vim plugins'
 nvim +PlugUpgrade +PlugUpdate +qall
 
