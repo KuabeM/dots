e89ceb3eb8967c7a7eb0ea2ade03a14ae2331ffb
Korbinian Maier <korbinian.maier@posteo.de>
2022-11-03 13:04:26 +0100

    nvim: fix breaking changes in cmp_nvim_lsp

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -74,8 +74,8 @@ cmp.setup({
 
 })
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+-- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local dap = require('dap')
 dap.adapters.lldb = {
