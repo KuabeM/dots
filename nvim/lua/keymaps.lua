083889d0bea142fbf8e6ffb94bbe43601b11f2db
Korbinian Maier <korbinian.maier@posteo.de>
2022-11-14 10:51:59 +0100

    nvim: add scrolling keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -6,3 +6,8 @@ vim.keymap.set("n", "<leader>fe", ":Explore<CR>", { silent = true, desc = ":Expl
 vim.keymap.set("n", "<leader>gblame", ":GitBlameToggle<CR>", { silent = true, desc = ":GitBlameToggle" })
 vim.keymap.set("n", "<leader>gsha", ":GitBlameCopySHA<CR>", { desc = ":GitBlameCopySHA" })
 
+-- scrolling
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true, desc = "<C-u>zz" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true, desc = "<C-d>zz" })
+vim.keymap.set("n", "n", "nzz", { silent = true, desc = "nzz" })
+vim.keymap.set("n", "N", "Nzz", { silent = true, desc = "Nzz" })
