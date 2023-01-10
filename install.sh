09b6a3a08ad12d3033c1a7000f6c9c3cf4722428
Korbinian Maier <korbinian.maier@franka.de>
2023-01-10 10:15:26 +0100

    install: fix packer and use init.lua

--- install.sh
+++ install.sh
@@ -19,14 +19,14 @@ done
 ln -nfs $src_dir/starship.toml $HOME/.config/starship.toml
 # Move nvim separately
 mkdir -p $HOME/.config/nvim
-ln -nfs $src_dir/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -nfs $src_dir/nvim/init.lua $HOME/.config/nvim/init.lua
 ln -nfs $src_dir/nvim/lua $HOME/.config/nvim/lua
 ln -nfs $src_dir/nvim/lua $HOME/.config/nvim/spell
 
 echo 'Install neovim plugin manager'
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-sh -c 'git clone --depth 1 https://github.com/wbthomason/packer.nvim\ \
+sh -c 'git clone --depth 1 https://github.com/wbthomason/packer.nvim \
     ~/.local/share/nvim/site/pack/packer/start/packer.nvim'
 echo 'Install vim plugins'
 nvim --headless +PlugUpgrade +PlugInstall +PlugUpdate +PackerInstall +PackerUpdate +qall
